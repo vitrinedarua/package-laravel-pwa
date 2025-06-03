0ee394a93bb03308b379713e477b144d139870ed
João Pedro Alves <joaopedroalv9@gmail.com>
2025-06-02 21:18:45 -0300

    removido console.log

--- resources/views/meta.blade.php
+++ resources/views/meta.blade.php
@@ -37,7 +37,6 @@
             scope: '.'
         }).then(function (registration) {
             // Registration was successful
-            console.log('Laravel PWA: ServiceWorker registration successful with scope: ', registration.scope);
         }, function (err) {
             // registration failed :(
             console.log('Laravel PWA: ServiceWorker registration failed: ', err);
